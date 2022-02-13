9176025ce7b85d03b0f3955d5735b2c15dca0584
1224police@likelion.org <1224police@likelion.org>
2022-02-13 17:26:21 +0900

    Basic websocket chat server

--- chat/src/main/java/com/websocket/chat/config/WebSockConfig.java
+++ chat/src/main/java/com/websocket/chat/config/WebSockConfig.java
@@ -1,5 +1,21 @@
 package com.websocket.chat.config;
 
-public class WebSockConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSocket
+public class WebSockConfig implements WebSocketConfigurer {
+    private final WebSocketHandler webSocketHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(webSocketHandler, "/ws/chat").setAllowedOrigins("*");
+    }
 }
