453be8d8d2c81eb77edc1b7c29758e798e8c9402
1224police@likelion.org <1224police@likelion.org>
2022-02-16 15:05:09 +0900

    update but there is no change

--- chat/src/main/java/com/websocket/chat/controller/ChatController.java
+++ chat/src/main/java/com/websocket/chat/controller/ChatController.java
@@ -2,6 +2,7 @@ package com.websocket.chat.controller;
 
 
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
 
@@ -16,10 +17,14 @@ public class ChatController {
 
  private final SimpMessageSendingOperations messagingTemplate;
 
+ //ENTER
  @MessageMapping("/chat/message")
  public void message(ChatMessage message) {
      if (ChatMessage.MessageType.ENTER.equals(message.getType()))
          message.setMessage(message.getSender() + "님이 입장하셨습니다.");
      messagingTemplate.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
  }
+ 
+ //TALK
+ 
 }
